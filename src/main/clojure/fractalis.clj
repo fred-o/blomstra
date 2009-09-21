3760a844bd364fc13adf0030547ac93be28ab949
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-09-21 17:48:33 +0200

    Plant curve.

--- src/main/clojure/fractalis.clj
+++ src/main/clojure/fractalis.clj
@@ -57,3 +57,20 @@
 		     :angle #(rad (* -45 (mod % 8)))))
 
 ;; (create-simple-frame (partial draw-dragon 14))
+
+(def plant
+     (lsystem
+      (:variables X
+		  F (forward))
+      (:constants + (right 25)
+		  - (left 25)
+		  < (remember)
+		  > (recall))
+      (:start X)
+      (:rules X -> F - < < X > + X > + F < + F X > - X
+	      F -> F F)))
+
+(def draw-plant
+  (create-draw-fn plant :angle -90))
+
+;(create-simple-frame (partial plant 4))
