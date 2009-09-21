ac1a52a47c0b8f31125dfa60f2b58ffe714e9d78
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-09-21 18:15:41 +0200

    gradient-fn

--- src/main/clojure/fractalis/ui.clj
+++ src/main/clojure/fractalis/ui.clj
@@ -6,6 +6,15 @@
 ;; General functions for managing graphic interfaces in Swing
 ;;
 
+(defn gradient-fn
+  ([c] (gradient-fn Color/white c))
+  ([c1 c2]
+     (let [gp (GradientPaint. 0 0 c1 500 1500 c2)]
+       (fn [g]
+	 (let [cb (.getClipBounds g)]
+	   (.setPaint g gp)
+	   (.fillRect g 0 0 (.getWidth cb) (.getHeight cb)))))))
+
 (defn create-mutable-frame []
   "Creates a JFrame and returns a vector of the frame, the set-update-fn and the repaint-fn."
   (let [frame (JFrame. "Fractalis")
@@ -24,18 +33,15 @@
 (defn create-simple-frame
   "Creates a JFrame with the given update-fn and bg-fn."
   ([update-fn]
-     (let [gp (GradientPaint. 0 0 Color/white 500 1500 Color/yellow)]
-       (create-simple-frame update-fn (fn [g w h]
-					(.setPaint g gp)
-					(.fillRect g 0 0 w h)
-					(.setColor g Color/black)))))
+     (create-simple-frame update-fn (gradient-fn Color/yellow)))
   ([update-fn bg-fn]
      (let [rh (RenderingHints. RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
 	   frame (JFrame. "Fractalis")
 	   c (proxy [Canvas] nil
 	       (paint [g]
 		      (.setRenderingHints g rh)
-		      (bg-fn g (proxy-super getWidth) (proxy-super getHeight))
+		      (bg-fn g)
+		      (.setColor g Color/black)
 		      (update-fn g)))]
        (doto frame
 	 (.setSize 500 500)
